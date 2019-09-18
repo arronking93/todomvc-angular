253f458e3d50babe32b7bd787335b721d2ce700e
arronking93 <sword9@126.com>
2019-09-18 12:32:08 +0800

    completed

--- README.md
+++ README.md
@@ -25,3 +25,35 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+---
+
+## TodoMVC-NG
+
+[TodoMVC](http://todomvc.com/)
+=> 下载模版和样式，然后自己实现功能(NG)
+
+1. 初始化项目并启用
+
+2. 将 `todomvc-angular\src\app\app.component.html` 文件内容替换
+
+3. 安装模板依赖的样式文件：
+
+```shell
+npm install todomvc-app-css
+```
+
+在 `todomvc-angular\src\styles.css` 文件中导入样式文件：
+
+```css
+/* You can add global styles to this file, and also import other style files */
+@import url('../node_modules/todomvc-app-css/index.css');
+```
+
+* 同样有todomvc-common模块的样式文件，但作用不显
+```css
+@import url('../node_modules/todomvc-common/base.css');
+```
+
+### 功能实现
+* cancelSave(取消时)会触发blurSave(失去焦点)，blurSave会保存编辑后的数据，应该对取消做进一步处理，这时人为设置编辑后数据为原数据(事件顺序为先取消，后失去焦点)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 const todos = [
-  { id: 1, content: 'eat', completed: true},
-  { id: 2, content: 'sing', completed: false},
-  { id: 3, content: 'code', completed: true}
+  {id: 1, content: 'eat', completed: true},
+  {id: 2, content: 'sing', completed: false},
+  {id: 3, content: 'code', completed: true}
 ];
 
 interface todosType {
-    id: number,
-    content: string,
-    completed: boolean
+  id: number,
+  content: string,
+  completed: boolean
 }
 
 @Component({
@@ -20,21 +20,46 @@ interface todosType {
 
 export class AppComponent {
   public todos: todosType[] = todos;
-  public filterTodos: todosType[] = todos;
   public selected: string = 'All';
   public currentId: number = 0;
   public currentContent: string = '';
 
+  ngOnInit() {
+    this.todos = JSON.parse(window.localStorage.getItem('todos')||'[]');
+    this.hashSwitch();
+    window.onhashchange = () => {
+      this.hashSwitch();
+    }
+  }
+
+  ngDoCheck() {
+    window.localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+
+  hashSwitch() {
+    const hash = window.location.hash.substr(1);
+      switch (hash) {
+        case '/':
+          this.selected = 'All';
+          break;
+        case '/active':
+          this.selected = 'Active';
+          break;
+        case '/completed':
+          this.selected = 'Completed';
+          break;
+      }
+  }
   addTodos(e) {
-    let last = this.todos[this.todos.length-1];
+    let last = this.todos[this.todos.length - 1];
     if (!e.target.value) return;
     this.todos.push({
-      id: last? last.id + 1 : 1,
+      id: last ? last.id + 1 : 1,
       content: e.target.value,
       completed: false
     });
     e.target.value = '';
-    console.log(this.todos[this.todos.length-1].id)
+    console.log(this.todos[this.todos.length - 1].id)
   }
 
   get toggleAll() {
@@ -48,7 +73,7 @@ export class AppComponent {
   }
 
   removeTodo(id) {
-    this.todos = this.todos.filter(item=>(item.id != id));
+    this.todos = this.todos.filter(item => (item.id != id));
   }
 
   toEditing(item) {
@@ -67,7 +92,7 @@ export class AppComponent {
     }
   }
 
-  cancelEdit(e, item) {
+  cancelEdit(e) {
     if (e.keyCode === 27) {
       this.currentId = 0;
       e.target.value = this.currentContent;
@@ -82,7 +107,7 @@ export class AppComponent {
   // 处理剩余条数
   get remaining() {
     let count = 0;
-    this.todos.forEach(item=>{
+    this.todos.forEach(item => {
       if (item.completed != true) {
         count++;
       }
@@ -91,26 +116,23 @@ export class AppComponent {
   }
 
   clearDone() {
-    this.todos = this.todos.filter(item=>!item.completed);
+    this.todos = this.todos.filter(item => !item.completed);
   }
 
   // 路由切换
-  allTodos() {
-    this.filterTodos = this.todos;
-    this.selected = 'All';
-  }
 
-  activeTodos() {
-    this.filterTodos = this.todos.filter(item=>{
-      return (!item.completed);
-    });
-    this.selected = 'Active';
-  }
-
-  completedTodos() {
-    this.filterTodos = this.todos.filter(item=>{
-      return (item.completed);
-    });
-    this.selected = 'Completed';
+  get filterTodos() {
+    switch (this.selected) {
+      case 'All':
+        return this.todos;
+      case 'Active':
+        return this.todos.filter(item => {
+          return (!item.completed);
+        });
+      case 'Completed':
+        return this.todos.filter(item => {
+          return (item.completed);
+        });
+    }
   }
 }
